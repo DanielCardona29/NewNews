f870c0edf55a6c8805b4e1811a469cda5b956ec8
Daniel Cardona <cardonacalderondaniel@gmail.com>
2020-09-01 00:33:21 -0500

    Ajustando el codigó de los comentarios

--- noticias/src/App/Comments/Comment.jsx
+++ noticias/src/App/Comments/Comment.jsx
@@ -1,182 +1,118 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../../Styles/App/Comments/Comments.scss';
 import CommController from '../../Controllers/CommentsController.js';
+import CommetnUser from './Presentar/CommentUser.jsx';
+import CommetnNoUser from './Presentar/CommentNoUser.jsx';
 
+class Comment extends React.Component {
 
-const CommentsController = new CommController();
+    constructor(prosp) {
+        super(prosp);
 
-const Comment = (props) => {
-    const [Like, setLike] = useState(props.isLiked)
-    const [countLikes, setcountLikes] = useState(props.likes)
-    const handleLiker = async () => {
+        this.state = {
+            Like: this.props.isLiked,
+            countLikes: this.props.likes
+        }
+        this.CommentsController = new CommController();
+
+    }
+
+    handleLiker = async () => {
         const userid = sessionStorage.getItem('userid');
-        if (Like) {
-            const consulta = await CommentsController.LikeController(props.comentario.id, props.newID, userid, false);
-            console.log(consulta);
-            setLike(false)
-            setcountLikes(countLikes - 1);
+        if (this.state.Like) {
+            await this.CommentsController.LikeController(this.props.comentario.id, this.props.newID, userid, false);
+            this.setState({
+                Like: false,
+                countLikes: this.state.countLikes - 1
+            })
         } else {
-            const consulta = await CommentsController.LikeController(props.comentario.id, props.newID, userid, true);
-            console.log(consulta);
-            setLike(true)
-            setcountLikes(countLikes + 1);
+            await this.CommentsController.LikeController(this.props.comentario.id, this.props.newID, userid, true);
+            this.setState({
+                Like: true,
+                countLikes: this.state.countLikes + 1
+            })
         }
     }
 
-    function getDate() {
+    getDate() {
         const d = new Date();
         const output = `${d.getFullYear()}/${d.getMonth()}/${d.getDay()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}:${d.getMilliseconds()}`;
         return output;
     }
 
-    const time = () => {
-        let fechaInicio = new Date(props.comentario.date).getTime();
-        let fechaFin = new Date(getDate()).getTime();
+    time = () => {
+        let fechaInicio = new Date(this.props.comentario.date).getTime();
+        let fechaFin = new Date(this.getDate()).getTime();
         let diff = fechaInicio - fechaFin;
         const date = Math.abs(diff / (1000 * 60 * 60 * 24)).toFixed(2);
         return date
     }
 
+    render() {
+        const userid = sessionStorage.getItem('userid')
+        if (this.props.comentario.idusercoment === userid) {
+            if (this.props.keyNum % 2 === 0) {
+                return <CommetnUser Like={this.state.Like}
+                    content={this.props.comentario.content}
+                    countLikes={this.state.countLikes}
+                    onLiked={() => this.handleLiker()}
+                    Like={this.state.Like}
+                    puntua={this.props.comentario.Punt}
+                    autor={this.props.autor.autor}
+                    onDelete={() => this.props.HasBedeleteComment(this.props.comentario.id, this.props.CommentPositon)}
+                    time={this.time()}
+                    isOscuro={true}
+                    id={this.props.comentario.id}
+                />;
+            } else {
+                return <CommetnUser Like={this.state.Like}
+                    content={this.props.comentario.content}
+                    countLikes={this.state.countLikes}
+                    onLiked={() => this.handleLiker()}
+                    Like={this.state.Like}
+                    puntua={this.props.comentario.Punt}
+                    autor={this.props.autor.autor}
+                    onDelete={() => this.props.HasBedeleteComment(this.props.comentario.id, this.props.CommentPositon)}
+                    time={this.time()}
+                    isOscuro={false}
+                    id={this.props.comentario.id}
 
-    const userid = sessionStorage.getItem('userid')
-
-    if (props.comentario.idusercoment === userid) {
-        if (props.keyNum % 2 === 0) {
-            let comment = (
-                <div className="commentWrapper oscuro">
-                    <div className="userCommetInfo">
-                        usuario: <label className="label">{props.autor.autor}</label>
-                Puntuación: <label className="label">{props.comentario.Punt}</label>
-            hace: <label className="label">{time()} dias</label>
-                        <div className="iconContent buttonAction edit" ></div>
-                        <div className="iconContent buttonAction deleat" onClick={ () => props.HasBedeleteComment( props.comentario.id, props.CommentPositon)}></div>
-                    </div>
-
-                    <div className="CommentContent">
-                        <p>{props.comentario.content}</p>
-                    </div>
-                    {
-                        (() => {
-                            if (Like) {
-                                return <div className="CommentStats">
-                                    <div className="like" onClick={() => handleLiker()}></div>
-                                    <label>{countLikes}</label>
-                                </div>
-                            } else {
-                                return <div className="CommentStats">
-                                    <div className="dontLike" onClick={() => handleLiker()}></div>
-                                    <label>{countLikes}</label>
-                                </div>
-                            }
-                        })()
-                    }
-                </div>
-            );
-            return comment;
+                />;
+            }
 
         } else {
-            let comment = (
-                <div className="commentWrapper">
-                    <div className="userCommetInfo">
-                        usuario: <label className="label">{props.autor.autor}</label>
-                        Puntuación: <label className="label">{props.comentario.Punt}</label>
-                        hace: <label className="label">{time()} dias</label>
-                        <div className="iconContent buttonAction edit"></div>
-                        <div className="iconContent buttonAction deleat" onClick={ () => props.HasBedeleteComment( props.comentario.id, props.CommentPositon)}></div>
-                    </div>
-                    <div className="CommentContent">
-                        <p>{props.comentario.content}</p>
-                    </div>
-                    {
-                        (() => {
-                            if (Like) {
-                                return <div className="CommentStats">
-                                    <div className="like" onClick={() => handleLiker()}></div>
-                                    <label>{countLikes}</label>
-                                </div>
-                            } else {
-                                return <div className="CommentStats">
-                                    <div className="dontLike" onClick={() => handleLiker()}></div>
-                                    <label>{countLikes}</label>
-                                </div>
-                            }
-                        })()
-                    }
-                </div>
-            );
-            return comment;
+            if (this.props.keyNum % 2 === 0) {
+                return <CommetnNoUser Like={this.state.Like}
+                    content={this.props.comentario.content}
+                    countLikes={this.state.countLikes}
+                    onLiked={() => this.handleLiker()}
+                    Like={this.state.Like}
+                    puntua={this.props.comentario.Punt}
+                    autor={this.props.autor.autor}
+                    time={this.time()}
+                    isOscuro={true}
+                    id={this.props.comentario.id}
 
-        }
+                />;
+            } else {
+                return <CommetnNoUser Like={this.state.Like}
+                    content={this.props.comentario.content}
+                    countLikes={this.state.countLikes}
+                    onLiked={() => this.handleLiker()}
+                    Like={this.state.Like}
+                    puntua={this.props.comentario.Punt}
+                    autor={this.props.autor.autor}
+                    time={this.time()}
+                    isOscuro={false}
+                    id={this.props.comentario.id}
 
-    } else {
+                />;
 
-        if (props.keyNum % 2 === 0) {
-            let comment = (
-                <div className="commentWrapper oscuro">
-                    <div className="userCommetInfo">
-                        usuario: <label className="label">{props.autor.autor}</label>
-                Puntuación: <label className="label">{props.comentario.Punt}</label>
-            hace: <label className="label">{time()} dias</label>
-
-                    </div>
-
-                    <div className="CommentContent">
-                        <p>{props.comentario.content}</p>
-                    </div>
-                    {
-                        (() => {
-                            if (Like) {
-                                return <div className="CommentStats">
-                                    <div className="like" onClick={() => handleLiker()}></div>
-                                    <label>{countLikes}</label>
-                                </div>
-                            } else {
-                                return <div className="CommentStats">
-                                    <div className="dontLike" onClick={() => handleLiker()}></div>
-                                    <label>{countLikes}</label>
-                                </div>
-                            }
-                        })()
-                    }
-                </div>
-            );
-            return comment;
-
-        } else {
-            let comment = (
-                <div className="commentWrapper">
-                    <div className="userCommetInfo">
-                        usuario: <label className="label">{props.autor.autor}</label>
-                        Puntuación: <label className="label">{props.comentario.Punt}</label>
-                        hace: <label className="label">{time()} dias</label>
-
-                    </div>
-                    <div className="CommentContent">
-                        <p>{props.comentario.content}</p>
-                    </div>
-                    {
-                        (() => {
-                            if (Like) {
-                                return <div className="CommentStats">
-                                    <div className="like" onClick={() => handleLiker()}></div>
-                                    <label>{countLikes}</label>
-                                </div>
-                            } else {
-                                return <div className="CommentStats">
-                                    <div className="dontLike" onClick={() => handleLiker()}></div>
-                                    <label>{countLikes}</label>
-                                </div>
-                            }
-                        })()
-                    }
-                </div>
-            );
-            return comment;
+            }
 
         }
 
     }
-
 }
 
 export default Comment;
---
+++ noticias/src/App/Comments/Presentar/CommentNoUser.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const CommetnNoUser = (props) => {
+    let comment = (
+        <div id={`ContenComent${props.id}`} className="commentWrapper">
+            <div className="userCommetInfo">
+                usuario: <label className="label">{props.autor}</label>
+                    Puntuación: <label className="label">{props.puntua}</label>
+                    hace: <label className="label">{props.time} dias</label>
+            </div>
+
+            <div className="CommentContent">
+                <p>{props.content}</p>
+            </div>
+            {
+                (() => {
+                    if (props.Like) {
+                        return <div className="CommentStats">
+                            <div className="like" onClick={props.onLiked}></div>
+                            <label>{props.countLikes}</label>
+                        </div>
+                    } else {
+                        return <div className="CommentStats">
+                            <div className="dontLike" onClick={props.onLiked}></div>
+                            <label>{props.countLikes}</label>
+                        </div>
+                    }
+                })()
+            }
+        </div>
+    );
+    if (props.isOscuro) {
+        setTimeout(() => {
+            document.getElementById(`ContenComent${props.id}`).classList = "commentWrapper oscuro"
+        }, 100)
+    }
+    return comment;
+}
+
+export default CommetnNoUser;
